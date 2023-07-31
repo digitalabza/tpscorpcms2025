bee93098dc4638709ef76327cdb1c221db501968
Matt <mattbradleyza@outlook.com>
2023-07-31 11:42:37 +0200

    updated settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -829,18 +829,8 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
-$databases['default']['default'] = array (
-  'database' => '2023jfm',
-  'username' => 'root',
-  'password' => '',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'driver' => 'mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+}
+
 $settings['config_sync_directory'] = 'sites/default/files/config_p6F_vabknBeJX4rJO3ael6cT2NSmFFF7VOHcpGZvvvEc9ohqkOP3hm0VJZS6vHil-wf-UV3K6g/sync';
